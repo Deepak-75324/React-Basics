906aae3ba8159600532e129ff073d4a7ad64b8fc
Dipak Mandal <deepakmandal98120@gmail.com>
2026-06-18 09:40:03 +0545

    creating context api project

--- 08miniContext/src/App.jsx
+++ 08miniContext/src/App.jsx
@@ -1,5 +1,8 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
+import UserContext from './context/UserContext'
 import UserContextProvider from './context/UserContextProvider'
+import Login from './components/Login'
+import Profile from './components/Profile'
 import './App.css'
 
 function App() {
@@ -8,6 +11,8 @@ function App() {
   return (
     <UserContextProvider>
     <h1>React with Context Api </h1>
+    <Login/>
+    <Profile />
     </UserContextProvider>
   )
 }
--- 08miniContext/src/components/Login.jsx
+++ 08miniContext/src/components/Login.jsx
@@ -1,19 +1,40 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import UserContext from '../context/UserContext'
 
 function Login() {
     const [username, setUsername] = useState('')
-    const [password, setPassword]= useState('')
-    const handleSbmit = () => {
+    const [password, setPassword] = useState('')
 
+    const { setUser } = useContext(UserContext)
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        setUser({ username, password })
     }
-    return(
+
+    return (
         <div>
             <h2>Login</h2>
-            <input typr="text" placeholder='username'/>
-            <input type='text' placeholder='password'/>
 
-            <button onClick={handleSbmit}><Submit></Submit></button>
+            <input
+                type="text"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                placeholder="Username"
+            />
+
+            {" "}
+
+            <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Password"
+            />
+
+            <button onClick={handleSubmit}>
+                Submit
+            </button>
         </div>
     )
 }
